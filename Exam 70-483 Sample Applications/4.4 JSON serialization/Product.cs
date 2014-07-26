7e56c2288b81c30d9418d5082d81a2b3ccc90514
bkowski <brett.rutkowski@live.com.au>
2014-07-26 22:36:08 +1000

    commit

---
+++ Exam 70-483 Sample Applications/4.4 JSON serialization/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace _4._4_JSON_serialization
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember] // Required for every field you want to serialize
+        private int _id;
+        [DataMember]
+        public string name;
+        [DataMember]
+        public decimal price;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
---
+++ Exam 70-483 Sample Applications/4.4 JSON serialization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace _4._4_JSON_serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prod = new Product();
+            prod.id = 1;
+            prod.name = "Kit Kat";
+            prod.price = 2.50m;
+
+            // Serialize
+            Stream stream = new FileStream("Product.json", FileMode.Create);
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Product));
+            ser.WriteObject(stream, prod); // Note: call WriteObject method instead of Serialize
+            stream.Close();
+
+            // Deserialize
+            Product prod2 = new Product();
+            Stream stream2 = new FileStream("Product.json", FileMode.Open);
+            DataContractJsonSerializer ser2 = new DataContractJsonSerializer(typeof(Product));
+            prod2 = (Product)ser.ReadObject(stream2); // Note: call ReadObject method instead of Deserialize
+            stream2.Close();
+        }
+    }
+}
