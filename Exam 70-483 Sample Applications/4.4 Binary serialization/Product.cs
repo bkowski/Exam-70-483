d429fe2093bbb1cc3d46d7ddd449feaf43a006e1
bkowski <brett.rutkowski@live.com.au>
2014-07-26 21:37:16 +1000

    commit

---
+++ Exam 70-483 Sample Applications/4.4 Binary serialization/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._4_Binary_serialization
+{
+    [Serializable]
+    class Product
+    {
+        private int _id;
+        public string name;
+        public decimal price;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
---
+++ Exam 70-483 Sample Applications/4.4 Binary serialization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _4._4_Binary_serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prod = new Product();
+            prod.name = "Candy Bar";
+            prod.price = 1.95m;
+            prod.id = 1;
+
+            IFormatter formatter = new BinaryFormatter();
+
+            // Serialize
+            Stream stream = new FileStream("Product.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, prod);
+            stream.Close();
+
+            // Deserialize
+            stream = new FileStream("Product.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+            Product prod2 = (Product)formatter.Deserialize(stream);
+            stream.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
