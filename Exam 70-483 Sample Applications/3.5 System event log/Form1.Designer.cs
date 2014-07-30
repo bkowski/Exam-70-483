4d2c915292d70e952539fdd418433d2df2951521
bkowski <brett.rutkowski@live.com.au>
2014-07-30 20:09:12 +1000

    commit

---
+++ Exam 70-483 Sample Applications/3.5 System event log/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace _3._5_System_event_log
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEvent = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Log:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ID:";
+            // 
+            // txtEvent
+            // 
+            this.txtEvent.Location = new System.Drawing.Point(27, 151);
+            this.txtEvent.Multiline = true;
+            this.txtEvent.Name = "txtEvent";
+            this.txtEvent.Size = new System.Drawing.Size(257, 111);
+            this.txtEvent.TabIndex = 3;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(86, 21);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(148, 20);
+            this.txtSource.TabIndex = 4;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(86, 51);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(148, 20);
+            this.txtLog.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(86, 85);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(148, 20);
+            this.txtId.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Event:";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(108, 280);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 8;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 332);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.txtEvent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "WriteToEventLog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEvent;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
+
---
+++ Exam 70-483 Sample Applications/3.5 System event log/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace _3._5_System_event_log
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            string source = txtSource.Text;
+            string log = txtLog.Text;
+            string message = txtEvent.Text;
+            int id = int.Parse(txtId.Text);
+
+            // create the source if necessary (requires admin privileges)
+            if(!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, log);
+            }
+
+            // write the log entry
+            EventLog.WriteEntry(source, message, EventLogEntryType.Information, id);
+
+            MessageBox.Show("OK");
+        }
+    }
+}
