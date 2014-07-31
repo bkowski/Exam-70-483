165335180584cf75061b8ace40759bb3582a3c83
bkowski <brett.rutkowski@live.com.au>
2014-07-31 21:08:39 +1000

    commit

---
+++ Exam 70-483 Sample Applications/3.2 ProtectedData/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _3._2_ProtectedData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEncrypt = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDecrypt = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Text to encrypt:";
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Location = new System.Drawing.Point(126, 52);
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.Size = new System.Drawing.Size(222, 20);
+            this.txtEncrypt.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(150, 103);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(119, 36);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(150, 226);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(119, 36);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Decrypted Text";
+            // 
+            // lblDecrypt
+            // 
+            this.lblDecrypt.AutoSize = true;
+            this.lblDecrypt.Location = new System.Drawing.Point(138, 289);
+            this.lblDecrypt.Name = "lblDecrypt";
+            this.lblDecrypt.Size = new System.Drawing.Size(0, 13);
+            this.lblDecrypt.TabIndex = 7;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(354, 339);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 383);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblDecrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtEncrypt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEncrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDecrypt;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Exam 70-483 Sample Applications/3.2 ProtectedData/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security.Cryptography;
+
+namespace _3._2_ProtectedData
+{
+    public partial class Form1 : Form
+    {
+        private byte[] encryptedData;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            // check textbox is not empty
+            if(string.IsNullOrEmpty(txtEncrypt.Text))
+            {
+                MessageBox.Show("Please enter some text to encrypt!");
+            }
+            else
+            {
+                // encrypt data
+                byte[] data = System.Text.Encoding.Default.GetBytes(txtEncrypt.Text);
+                try
+                {
+                    encryptedData = ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
+                    MessageBox.Show("Encryption Successful!");
+                }
+                catch(CryptographicException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            // decrypt data and display in label
+            lblDecrypt.Text = System.Text.Encoding.Default.GetString(ProtectedData.Unprotect(encryptedData, null, DataProtectionScope.CurrentUser));
+        }
+
+    }
+}
