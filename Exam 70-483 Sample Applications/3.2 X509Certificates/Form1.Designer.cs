045a1d432ce07421afbb7e1db5a5387b349e4142
bkowski <brett.rutkowski@live.com.au>
2014-07-31 20:31:20 +1000

    commit

---
+++ Exam 70-483 Sample Applications/3.2 X509Certificates/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _3._2_X509Certificates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtCertInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(139, 70);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(152, 57);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Show Certificates";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtCertInfo
+            // 
+            this.txtCertInfo.Location = new System.Drawing.Point(39, 155);
+            this.txtCertInfo.Multiline = true;
+            this.txtCertInfo.Name = "txtCertInfo";
+            this.txtCertInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCertInfo.Size = new System.Drawing.Size(398, 188);
+            this.txtCertInfo.TabIndex = 1;
+            this.txtCertInfo.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Shows X509Certificates info";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(362, 381);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 435);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCertInfo);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox txtCertInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Exam 70-483 Sample Applications/3.2 X509Certificates/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace _3._2_X509Certificates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            txtCertInfo.Text = string.Empty;
+
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadOnly);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Friendly Name \t\t\t\t Expiration Date");
+
+            foreach(X509Certificate2 certificate in store.Certificates)
+            {
+                sb.AppendFormat("{0}\t{1}{2}", certificate.FriendlyName, certificate.NotAfter, Environment.NewLine);
+            }
+            store.Close();
+
+            txtCertInfo.Text = sb.ToString();
+        }
+    }
+}
