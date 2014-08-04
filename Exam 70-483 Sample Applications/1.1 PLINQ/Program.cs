4e07630675917f7f9cadfb0c1097873052896389
bkowski <brett.rutkowski@live.com.au>
2014-08-04 20:39:38 +1000

    commit

---
+++ Exam 70-483 Sample Applications/1.1 PLINQ/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1_PLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OrderedParallelQuery();
+            AsSequential();
+        }
+
+        private static void UsingAsParallel()
+        {
+            var numbers = Enumerable.Range(0, 10000000);
+            var parallelResult = numbers.AsParallel()
+                .Where(i => i % 2 == 0).ToArray();
+        }
+
+        private static void OrderedParallelQuery()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            var result = numbers.AsParallel().AsOrdered()
+                .Where(i => i % 2 == 0)
+                .ToArray();
+
+            foreach(int num in result)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+
+        private static void AsSequential()
+        {
+            var numbers = Enumerable.Range(0, 20);
+            var parallelResult = numbers.AsParallel().AsOrdered()
+                .Where(i => i % 2 == 0).AsSequential();
+
+            foreach(int i in parallelResult.Take(5))
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+
+        private static void UsingForAll()
+        {
+            var numbers = Enumerable.Range(0, 20);
+            var parallelResult = numbers.AsParallel()
+                .Where(i => i % 2 == 0);
+
+            parallelResult.ForAll(e => Console.WriteLine(e));
+        }
+    }
+}
