0c0790c87d34b7322924faa9929e200f7122b27b
bkowski <brett.rutkowski@live.com.au>
2014-08-05 23:12:12 +1000

    commit

---
+++ Exam 70-483 Sample Applications/1.4 Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4_Delegates
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UseDelegate();
+            Console.ReadLine();
+        }
+
+        public delegate int Calculate(int x, int y);
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static void UseDelegate()
+        {
+            Calculate calc = Add;
+            Console.WriteLine(calc(5, 10));
+
+            calc = Multiply;
+            Console.WriteLine(calc(4, 6));
+        }
+    }
+}
---
+++ Exam 70-483 Sample Applications/1.4 Events/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4_Events
+{
+    // a class to hold information about the event
+    public class TimeInfoEventArgs : EventArgs
+    {
+        public int hour;
+        public int minute;
+        public int second;
+
+        public TimeInfoEventArgs(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+    }
+
+    // the publisher: class other classes will observe
+    public class Clock
+    {
+        private int hour;
+        private int minute;
+        private int second;
+
+        // the delegate the subscribers must implement
+        public delegate void SecondChangedHandler(object clock, TimeInfoEventArgs timeInformation);
+        // an instance of the delegate using event keyword
+        public event SecondChangedHandler SecondChanged;
+
+        // set the clock running
+        public void Run()
+        {
+            // infinite loop
+            for(; ; )
+            {
+                System.Threading.Thread.Sleep(100);
+
+                // get the current time
+                System.DateTime dt = DateTime.Now;
+                // if the second has changed notify the subscribers
+                if(dt.Second != this.second)
+                {
+                    // create the TimeInfoEventArgs object to pass to the subscriber
+                    TimeInfoEventArgs timeInformation = new TimeInfoEventArgs(dt.Hour, dt.Minute, dt.Second);
+
+                    // if anyone has subscribed notify them
+                    if(SecondChanged != null)
+                    {
+                        SecondChanged(this, timeInformation);
+                    }
+                }
+
+                // update the state
+                this.hour = dt.Hour;
+                this.minute = dt.Minute;
+                this.second = dt.Second;
+            }
+        }
+    }
+
+    // a subscriber
+    public class DisplayClock
+    {
+        // given a clock, subscribe to its SecondChangedHandler event
+        public void Subscribe(Clock theClock)
+        {
+            theClock.SecondChanged += new Clock.SecondChangedHandler(TimeHasChanged);
+        }
+
+        // the method that implements the delegated functionality
+        public void TimeHasChanged(object clock, TimeInfoEventArgs ti)
+        {
+            Console.WriteLine("Current time: {0}:{1}:{2}", ti.hour, ti.minute, ti.second);
+        }
+    }
+
+    public class Tester
+    {
+        public void Run()
+        {
+            // create a new clock
+            Clock theClock = new Clock();
+
+            // create the display and tell it to subscribe to the clock just created
+            DisplayClock displayClock = new DisplayClock();
+            displayClock.Subscribe(theClock);
+
+            // get the clock started
+            theClock.Run();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Tester test = new Tester();
+            test.Run();
+        }
+    }
+}
