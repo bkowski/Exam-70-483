eabe9c5afb1f866363425646959a354ea889b5df
bkowski <brett.rutkowski@live.com.au>
2014-08-09 17:01:43 +1000

    commit

---
+++ Exam 70-483 Sample Applications/2.6 IDisposable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._6_IDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var file = new OpenFile("myFile.txt"))
+            {
+
+            }
+        }
+    }
+}
