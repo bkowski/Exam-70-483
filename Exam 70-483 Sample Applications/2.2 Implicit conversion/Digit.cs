b69445d899cce0acbc1476ecc3ecb7acd29b399e
bkowski <brett.rutkowski@live.com.au>
2014-08-09 23:44:57 +1000

    commit

---
+++ Exam 70-483 Sample Applications/2.2 Implicit conversion/Digit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2_Implicit_conversion
+{
+    class Digit
+    {
+        public Digit(double d)
+        {
+            val = d;
+        }
+
+        public double val;
+
+        public static implicit operator double(Digit d)
+        {
+            return d.val;
+        }
+
+        public static implicit operator Digit(double d)
+        {
+            return new Digit(d);
+        }
+    }
+}
---
+++ Exam 70-483 Sample Applications/2.2 Implicit conversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2_Implicit_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Digit dig = new Digit(7);
+            // implicit conversion
+            double num = dig;
+
+            // implicit conversion
+            Digit dig2 = 12;
+
+            Console.WriteLine("num = {0}, dig2 = {1}", num, dig2.val);
+            Console.ReadLine();
+        }
+    }
+}
