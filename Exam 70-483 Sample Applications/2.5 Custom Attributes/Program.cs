a7b4113fdf9ccc7801c17fa28e9cfef189baebbd
bkowski <brett.rutkowski@live.com.au>
2014-08-09 11:38:32 +1000

    commit

---
+++ Exam 70-483 Sample Applications/2.5 Custom Attributes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5_Custom_Attributes
+{
+    class Program
+    {
+        [Programmer("Brett Rutkowski")] // custom attribute marks this class's programmer as Brett Rutkowski and version 1.0
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Exam 70-483 Sample Applications/2.5 Custom Attributes/Programmer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._5_Custom_Attributes
+{
+    // this allows the attribute to only be applied to class and struct declarations
+    // AllowMultiple allows multiple attributes of the same type
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    class Programmer : Attribute // custom attributes must inherit from Attribute
+    {
+        private string name;
+        public double version;
+
+        public Programmer(string name)
+        {
+            this.name = name;
+            version = 1.0;
+        }
+    }
+}
