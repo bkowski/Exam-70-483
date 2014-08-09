2d83458ab106d92ee3dccd735eb718ffbccc1a4a
bkowski <brett.rutkowski@live.com.au>
2014-08-09 12:39:30 +1000

    commit

---
+++ Exam 70-483 Sample Applications/2.5 CodeDOM/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using System.IO;
+
+namespace _2._5_CodeDOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeCompileUnit ccu = HelloWorldCodeDOM();
+
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+
+            using(StreamWriter sw = new StreamWriter("HelloWorld.cs", false))
+            {
+                IndentedTextWriter tw = new IndentedTextWriter(sw, "    ");
+                provider.GenerateCodeFromCompileUnit(ccu, tw, new CodeGeneratorOptions());
+                tw.Close();
+            }
+        }
+
+        static CodeCompileUnit HelloWorldCodeDOM()
+        {
+            CodeCompileUnit compileUnit = new CodeCompileUnit();
+            CodeNamespace myNamespace = new CodeNamespace("MyNamespace");
+            myNamespace.Imports.Add(new CodeNamespaceImport("System"));
+            CodeTypeDeclaration myClass = new CodeTypeDeclaration("MyClass");
+            CodeEntryPointMethod start = new CodeEntryPointMethod();
+            CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(new CodeTypeReferenceExpression("Console"), "WriteLine", new CodePrimitiveExpression("Hello World!"));
+
+            compileUnit.Namespaces.Add(myNamespace);
+            myNamespace.Types.Add(myClass);
+            myClass.Members.Add(start);
+            start.Statements.Add(cs1);
+
+            return compileUnit;
+        }
+    }
+}
--- Exam 70-483 Sample Applications/2.5 Custom Attributes/Program.cs
+++ Exam 70-483 Sample Applications/2.5 Custom Attributes/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace _2._5_Custom_Attributes
 {
+    [Programmer("Brett Rutkowski")] // custom attribute marks this class's programmer as Brett Rutkowski and version 1.0
     class Program
-    {
-        [Programmer("Brett Rutkowski")] // custom attribute marks this class's programmer as Brett Rutkowski and version 1.0
+    {       
         static void Main(string[] args)
         {
         }
