f70054bed680f5c47b8746d8ebe7b25092eed497
bkowski <brett.rutkowski@live.com.au>
2014-08-11 12:02:04 +1000

    commit

---
+++ Exam 70-483 Sample Applications/3.2 AES Symmetric Algorithm/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Security.Cryptography;
+
+namespace _3._2_AES_Symmetric_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "Some top secret FBI data";
+
+            using(SymmetricAlgorithm symmetricAlgorithm = new AesManaged())
+            {
+                byte[] encrypted = Encrypt(symmetricAlgorithm, original);
+                string roundtrip = Decrypt(symmetricAlgorithm, encrypted);
+
+                Console.WriteLine("Original: {0}", original);
+                Console.WriteLine("Round Trip: {0}", roundtrip);
+                Console.ReadLine();
+            }
+        }
+
+        static byte[] Encrypt(SymmetricAlgorithm aesAlg, string plainText)
+        {
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using(MemoryStream msEncrypt = new MemoryStream())
+            {
+                using(CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using(StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return msEncrypt.ToArray();
+                }
+            }
+        }
+
+        static string Decrypt(SymmetricAlgorithm aesAlg, byte[] cipherText)
+        {
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using(MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using(CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using(StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
