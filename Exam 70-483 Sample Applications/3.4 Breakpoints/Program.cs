ff5d77c0fe75e3d5b0e5875f1c92712131c5d44e
bkowski <brett.rutkowski@live.com.au>
2014-08-15 17:13:23 +1000

    commit

---
+++ Exam 70-483 Sample Applications/3.4 Breakpoints/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._4_Breakpoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(int num in Enumerable.Range(1,50))
+            {
+                // breakpoint is set to fire when hit count equals 20
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
