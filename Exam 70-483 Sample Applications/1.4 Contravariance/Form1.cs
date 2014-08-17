54ce31485cad5a56d1c31597d375c9dbcd540b2a
bkowski <brett.rutkowski@live.com.au>
2014-08-17 22:43:37 +1000

    commit

---
+++ Exam 70-483 Sample Applications/1.4 Contravariance/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1._4_Contravariance
+{
+    public partial class Form1 : Form
+    {
+        // event handler
+        private void MultiHandler(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.button1.KeyDown += this.MultiHandler;
+
+            this.button1.Click += this.MultiHandler;
+        }
+    }
+}
---
+++ Exam 70-483 Sample Applications/1.4 Covariance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4_Covariance
+{
+    class Mammals { }
+    class Dogs : Mammals { }
+    class Program
+    {
+        // define the delegate
+        public delegate Mammals HandlerMethod();
+
+        public static Mammals MammalsHandler()
+        {
+            return null;
+        }
+
+        public static Dogs DogsHandler()
+        {
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            HandlerMethod handlerMammals = MammalsHandler;
+
+            // Covariance enables this assignment
+            HandlerMethod handlerDogs = DogsHandler;
+        }
+    }
+}
