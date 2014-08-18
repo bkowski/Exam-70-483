b0fd533edeca6a817736b1b454a1795084d34213
bkowski <brett.rutkowski@live.com.au>
2014-08-18 22:20:17 +1000

    commit

---
+++ Exam 70-483 Sample Applications/1.4 CarDelegate/Car.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4_CarDelegate
+{
+    public class Car
+    {
+        // Delegate type
+        public delegate void CarEngineHandler(string msgForCaller);
+        // Member variable of this delegate
+        private CarEngineHandler listOfHandlers;
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers += methodToCall;
+        }
+        public void Accelerate(int delta)
+        {
+            if(carIsDead)
+            {
+                if(listOfHandlers != null)
+                {
+                    listOfHandlers("Sorry, this car is dead");
+                }
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if(10 == (CurrentSpeed - MaxSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful, gonna blow");
+                }
+                if(CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                }
+                else
+                {
+                    Console.WriteLine("Current speed = {0}", CurrentSpeed);
+                }
+            }
+        }
+
+        // internal state data
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is the car alive or dead
+        public bool carIsDead;
+
+        // Class constructors
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSpd, int curSpd)
+        {
+            CurrentSpeed = curSpd;
+            MaxSpeed = maxSpd;
+            PetName = name;
+
+        }
+    }
+}
---
+++ Exam 70-483 Sample Applications/1.4 CarDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._4_CarDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Delegates as event enablers *****\n");
+
+            // First make a car object
+            Car car1 = new Car("Slug Bug", 100, 10);
+
+            // Now tell the car which method to call when it wants to send us messages
+            car1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+
+            // Speed up, this will trigger the events
+            Console.WriteLine("***** Speeding Up *****");
+            for(int i = 0; i < 6; i++)
+            {
+                car1.Accelerate(20);
+                Console.ReadLine();
+            }            
+        }
+
+        // This is the target for incomming events
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n***** Message from car object *****");
+            Console.WriteLine("-> {0}", msg);
+            Console.WriteLine("*************************************\n");
+        }
+    }
+}
